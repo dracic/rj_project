9d747496ee23802a93450917dbfcc1242772cd0c
grubicro <grubicro@gmail.com>
2017-07-22 11:50:27 +0200

    added home.html and abou.html

--- rjesenja/upis/urls.py
+++ rjesenja/upis/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.GJList.as_view(), name='gj_list'),
+    url(r'^$', views.HomeView.as_view(), name='home'),
+    url(r'^about/$',views.AboutView.as_view(),name='about'),
+    url(r'^pregled/$', views.GJList.as_view(), name='gj_list'),
     url(r'^create/$', views.CreateGJ.as_view(), name='create_gj'),
     url(r'^edit/(?P<pk>\d+)/$', views.EditGJ.as_view(), name='edit_gj'),
     url(r'^delete/(?P<pk>\d+)/$', views.DeleteGJ.as_view(), name='delete_gj'),
--- rjesenja/upis/views.py
+++ rjesenja/upis/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, DeleteView, ListView, UpdateView
+from django.views.generic import (CreateView, DeleteView,
+                                ListView, UpdateView,
+                                TemplateView)
 from upis.models import GJ
 from upis.forms import GJForm
+
+
+class HomeView(TemplateView):
+    template_name = "home.html"
+
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
 
 
 class GJList(ListView):
